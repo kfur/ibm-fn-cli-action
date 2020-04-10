3c8617be52ebcb4662121ec57b83b1fbe5db586e
Denis <deniskovpen@gmail.com>
2020-04-10 16:42:33 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 ibmcloud login -u "$USERNAME" -p "$PASSWORD" -r "$CF_REGION"
 ibmcloud target --cf
+ibmcloud plugin install -f cloud-functions
 ibmcloud fn action update "$APP_NAME" --docker "$CF_DOCKER_IMAGE" "$MAIN_SCRIPT_NAME"
 
