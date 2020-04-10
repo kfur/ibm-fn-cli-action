146bb2b9f70b7876dc4ac8df6fa0cb8c5ad68d21
Denis <deniskovpen@gmail.com>
2020-04-10 16:35:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ibmcloud cf login -a "$CF_API_ENDPOINT" -o "$ORG" -s "$SPACE" -u "$USERNAME" -p "$PASSWORD"
+ibmcloud login -u "$USERNAME" -p "$PASSWORD" -r "$CF_REGION"
 ibmcloud target --cf
 ibmcloud fn action update "$APP_NAME" --docker "$CF_DOCKER_IMAGE" "$MAIN_SCRIPT_NAME"
 
